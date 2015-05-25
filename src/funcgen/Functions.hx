d439c4f54f6de27e0e496909f740380ac0026d03
francescoagati <franescoagati1975@gmail.com>
2015-05-25 22:42:24 +0200

    add fn3 fn4 fn5

--- src/funcgen/Functions.hx
+++ src/funcgen/Functions.hx
@@ -14,8 +14,35 @@ class Functions {
   }
 
   macro public static function fn2(filter:Expr) {
-    var new_filter = filter.substitute({ "_1": macro el1 });
-    new_filter = new_filter.substitute({ "_2": macro el2 });
+    var new_filter = filter.substitute({ "_1": macro el1 }).substitute({ "_2": macro el2 });
     return macro function(el1,el2) { return $new_filter;  };
   }
+
+  macro public static function fn3(filter:Expr) {
+    var new_filter = filter
+      .substitute({ "_1": macro el1 })
+      .substitute({ "_2": macro el2 })
+      .substitute({ "_3": macro el3 });
+    return macro function(el1,el2,el3) { return $new_filter;  };
+  }
+
+  macro public static function fn4(filter:Expr) {
+    var new_filter = filter
+      .substitute({ "_1": macro el1 })
+      .substitute({ "_2": macro el2 })
+      .substitute({ "_3": macro el3 })
+      .substitute({ "_4": macro el4 });
+    return macro function(el1,el2,el3,el4) { return $new_filter;  };
+  }
+
+  macro public static function fn5(filter:Expr) {
+    var new_filter = filter
+      .substitute({ "_1": macro el1 })
+      .substitute({ "_2": macro el2 })
+      .substitute({ "_3": macro el3 })
+      .substitute({ "_4": macro el4 })
+      .substitute({ "_5": macro el5 });
+    return macro function(el1,el2,el3,el4,el5) { return $new_filter;  };
+  }
+
 }
