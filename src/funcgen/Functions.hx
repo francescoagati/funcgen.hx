515d1bbd4cf36f2c067c51ffc1c51caca6d40f5b
francesco agati <francescoagati1975@gmail.com>
2015-05-25 13:30:02 +0200

    Update Functions.hx

--- src/funcgen/Functions.hx
+++ src/funcgen/Functions.hx
@@ -1,21 +1,21 @@
 package funcgen;
 
 #if macro
-	import haxe.macro.Context;
-	import haxe.macro.Expr;
-	using tink.MacroApi;
+  import haxe.macro.Context;
+  import haxe.macro.Expr;
+  using tink.MacroApi;
 #end
 
 class Functions {
 
   macro public static function fn(filter:Expr) {
-		var new_filter = filter.substitute({ "_": macro el });
+    var new_filter = filter.substitute({ "_": macro el });
     return macro function(el) { return $new_filter;  };
-	}
+  }
 
-	macro public static function fn2(filter:Expr) {
-		var new_filter = filter.substitute({ "_1": macro el1 });
-		new_filter = new_filter.substitute({ "_2": macro el2 });
-		return macro function(el1,el2) { return $new_filter;  };
-	}
+  macro public static function fn2(filter:Expr) {
+   var new_filter = filter.substitute({ "_1": macro el1 });
+   new_filter = new_filter.substitute({ "_2": macro el2 });
+   return macro function(el1,el2) { return $new_filter;  };
+  }
 }
