32d5ba6a7a0b6de416e596439da2b962ace2362f
francesco agati <francescoagati1975@gmail.com>
2015-05-25 13:30:15 +0200

    Update Functions.hx

--- src/funcgen/Functions.hx
+++ src/funcgen/Functions.hx
@@ -14,8 +14,8 @@ class Functions {
   }
 
   macro public static function fn2(filter:Expr) {
-   var new_filter = filter.substitute({ "_1": macro el1 });
-   new_filter = new_filter.substitute({ "_2": macro el2 });
-   return macro function(el1,el2) { return $new_filter;  };
+    var new_filter = filter.substitute({ "_1": macro el1 });
+    new_filter = new_filter.substitute({ "_2": macro el2 });
+    return macro function(el1,el2) { return $new_filter;  };
   }
 }
